74ce2e7741084f85a8e83f9a8693eec61f60f4fd
Olexiy Reshetnyak <icolexiy.reshetnyak@amwell.com>
2023-11-22 09:02:03 -0700

    fifth app, initial commit

---
+++ fifth_app_shopping_list/README.md
@@ -0,0 +1,3 @@
+# fifth_app_shopping_list
+
+A new Flutter project.
---
+++ fifth_app_shopping_list/android/app/src/main/kotlin/com/example/fifth_app_shopping_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fifth_app_shopping_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
